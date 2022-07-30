d4e391f6429b5b9493f9e66bfee8a2ff45afe926
Russ <russellkohler8@gmail.com>
2022-07-30 15:28:59 -0500

    Completed Part 1: Loan Calculator functionality and Jasmine tests.

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,20 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect(calculateMonthlyPayment(value = {
+    amount: 200, 
+    years: 2, 
+    rate: 0.2}
+  )).toEqual('10.18');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const value = {
+    amount: 200, 
+    years: 2, 
+    rate: 0.2
+  }
+  expect(calculateMonthlyPayment(value).endsWith('.18')).toBe(true);
 });
 
 /// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,33 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  document.getElementById("loan-amount").value = 200;
+  document.getElementById("loan-years").value = 2;
+  document.getElementById("loan-rate").value = 0.2;
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const values = getCurrentUIValues();
+  const monthCalc = calculateMonthlyPayment(values);
+  updateMonthly(monthCalc);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const P = values.amount;
+  const i = values.rate / 12;
+  const n = values.years * 12;
+  let monthlyPayment = (P * i) / (1 - Math.pow((1 + i), -n));
+  return monthlyPayment.toFixed(2).toString();
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  document.getElementById("monthly-payment").innerText = monthly;
 }
